7e575fe572a48b5642e4ce686b9f248dbbe8a769
adriancolumb2025-ui <adriancolumb2025@gmail.com>
2026-03-13 01:20:29 +1100

    Update graph-test.js

--- graph-test.js
+++ graph-test.js
@@ -1,1 +1,8 @@
+console.log("Testing Microsoft Graph developer environment");
 
+async function testGraphConnection() {
+  const status = "Microsoft Graph placeholder test running";
+  console.log(status);
+}
+
+testGraphConnection();
